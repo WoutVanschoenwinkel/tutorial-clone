b899d9b12f638442b43f2d771998da656a9fb5d3
WoutVanschoenwinkel <Wout.vanschoenwinkel@student.pxl.be>
2026-02-12 14:45:51 +0100

    tekstkleur naar cyan

--- ClonedProject/ClonedProject/Program.cs
+++ ClonedProject/ClonedProject/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Proficiat, je hebt je eerste clone gemaakt!");
+            Console.ResetColor();
         }
     }
 }
